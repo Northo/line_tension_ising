4306ec812823d130fcd025d9004bd249142f8bd8
Thorvald Ballestad <thorvalb@stud.ntnu.no>
2020-05-04 13:40:42 +0200

    CartesianIndex

---
+++ utils.jl
@@ -0,0 +1,73 @@
+"""Simulation flow:
+ . Given a nx by ny array of elements with -1 or +1.
+ . Select a element at random
+ . For that element, look up Delta H
+ . Compare Delta H to acceptance function.
+    - If accepted do change, if not reject
+"""
+
+function get_random_Hamiltonian(Nx::Int, Ny::Int)
+    H = rand(Int, (Ny, Nx))
+    return H
+end
+
+
+function step!(
+    H::AbstractArray,
+    N::Int,
+    T,
+    ir::AbstractVector,
+    il::AbstractVector,
+    iu::AbstractVector,
+    id::AbstractVector,
+)
+    """Performs one step of the Minnapolis algorithm.
+    Attempt to flip one spin
+
+    ir, il, iu, id are lookups for index right, left, up, down
+
+    TODO:
+     . Instead of evaluating exp(-DH/T), lookup preevaluated
+     . Consider if one should change order of operations,
+    to reduce operations. Ie. calculate acceptance_criterion
+    before r, then throw away at once if DH < 0.
+     . Find out how efficient CartesianIndex is
+     . Implement periodic boundary conditions
+    """
+    s_i = rand(1:N)  # Spin to flip
+    s_i_cartesian = CartesianIndices(H)[s_i]
+    s_i_x, s_i_y = Tuple(s_i_cartesian)
+    neighbors = [H[idx, idy] for (idx, idy) in [
+        (ir[s_i_x], s_i_y),  # Right
+        (il[s_i_x], s_i_y),  # Left
+        (s_i_x, iu(s_i_y)),  # Up
+        (s_i_x, id(s_i_y)),  # Down
+    ]]
+
+    neighbors_indices = [s_i_cartesian + i for i in [
+        CartesianIndex(1, 0),
+        CartesianIndex(-1, 0),
+        CartesianIndex(0, 1),
+        CartesianIndex(0, -1),
+    ]]  # Neighbors right, left, above, below
+    r = rand()  # Random number r in [0,1)
+
+    spin = H[s_i]
+    neighbors = H[neighbors_indices]
+    # TODO: Looup exponential
+    delta_H = 2*spin*sum(neighbors)
+    acceptance_criterion = exp(-delta_H/T)
+    if acceptance_criterion > r
+        H[s_i] = -spin
+    end
+end
+
+Nx = 5
+Ny = 5
+N = Nx * Ny
+T = 1
+
+H = get_random_Hamiltonian(Nx, Ny)
+println(H)
+step!(H, N, T)
+println(H)
