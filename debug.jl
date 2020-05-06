1bde0e080dd15986ce3953cd74052c552e0ea9c0
Thorvald Ballestad <thorvalb@stud.ntnu.no>
2020-05-06 17:41:01 +0200

    Adds debug.jl

---
+++ debug.jl
@@ -0,0 +1,37 @@
+include("utils.jl")
+
+#########
+# Setup #
+#########
+N_sweeps = 100000
+Nx, Ny = 20, 20
+T = 1
+
+#system = :pp
+system = :torus
+
+
+################
+# Calculations #
+################
+if system == :pp
+    H = get_pp_hamiltonian(Nx, Ny)
+    ir, il, iu, id = get_pp_index_vectors(Nx, Ny)
+    difference_function = pp_pn_difference
+elseif system == :torus
+    H = get_random_hamiltonian(Nx, Ny)
+    ir, il, iu, id = get_torus_index_vectors(Nx, Ny)
+    difference_function = torus_klein_difference
+else
+    throw(ArgumentError("Invalid system"))
+end
+
+# Initial energies
+@time simulate!(
+    H,
+    Nx, Ny,
+    T,
+    N_sweeps,
+    ir, il, iu, id,
+    difference_function=difference_function,
+)
