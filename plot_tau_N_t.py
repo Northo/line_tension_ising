54d0de54787bac728ec2ff3faf60b7249d70fa7b
Thorvald Ballestad <thorvalb@stud.ntnu.no>
2020-05-07 17:44:14 +0200

    Adds script for plotting tau/t vs Ntau

---
+++ plot_tau_N_t.py
@@ -0,0 +1,91 @@
+"""Plots tau vs. T for various N"""
+
+import numpy as np
+import pandas
+import matplotlib.pyplot as plt
+import sys
+import getopt
+
+newparams = {'axes.labelsize': 15,
+             'axes.linewidth': 1,
+             'lines.linewidth': 1.5, 
+             'figure.figsize': (9, 8),
+             'ytick.labelsize': 15,
+             'xtick.labelsize': 15,
+             'ytick.major.pad': 5,
+             'xtick.major.pad': 5,
+             'legend.fontsize': 15,
+             'legend.frameon': True, 
+             'legend.handlelength': 1.5,
+             'axes.titlesize': 20,
+             'mathtext.fontset': 'stix',
+             'font.family': 'STIXGeneral'}
+plt.rcParams.update(newparams)
+
+Tc = 2.26919  # Onsager's critical temp
+filename = "datadir/T_N_new.dat"
+
+def onsager(T):
+    return 2 - T*Tc*np.log(1/np.tanh(1/(T*Tc)))
+
+def t(T):
+    return 1 - T/Tc
+
+options, rest = getopt.gnu_getopt(sys.argv[1:], 't:', ["title=", "save=", "logy"])
+
+
+title = ""
+ts = ""
+save = False
+for opt, arg in options:
+    if opt == "--title":
+        title = arg
+    elif opt in ("--save"):
+        save = True
+        figname = arg
+    elif opt == "--logy":
+        plt.yscale("log")
+    elif opt == "-t":
+        ts = arg
+
+markers = ["D", "s"]
+
+df = pandas.read_csv(filename, delimiter="\t")
+df["t"] = t(df["T"])
+# Remove values over critica
+df = df[df["t"]>0]
+
+def plot(df, marker="s", label=""):
+    y = df["tau"]/df["t"]
+    x = 1/(df["N"]*df["t"])
+    plt.scatter(x, y, marker=marker, label=label)
+
+# If given ts, group
+i = 0  # For markers
+markers = ["o", "v", "s", "^", "x", "+", "<", ">"]
+if ts:
+    ts = ts.split(",")
+    ts = [float(t) for t in ts]
+    groups = df.groupby(["t"])
+    for t, group in groups:
+        # Hack to avoid round off trouble
+        t = float(f"{t:.4f}")
+        if t in ts:
+            plot(group, marker=markers[i], label=t)
+            i+=1
+else:
+    plot(df)
+
+if title:
+    plt.title(title)
+
+plt.plot([0, 5], [3.99, 25])
+plt.xlabel("$1/Nt$")
+plt.ylabel("$\\frac{\\tau}{t}$")
+plt.axhline(y=0, linestyle="dashed", color="gray", linewidth=0.2)
+
+plt.axvline(x=0, linestyle="dashed", color="gray", linewidth=0.2)
+plt.legend()
+if save:
+    plt.savefig(figname)
+plt.show()
